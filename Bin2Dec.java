17262a93c45e0419cd4559dc2ca1b1ec40d242cb
Sophia G <137671339+sopgorma@users.noreply.github.com>
2025-03-30 05:27:07 -0400

    Bin2Dec.java

--- Bin2Dec.java
+++ Bin2Dec.java
@@ -10,7 +10,7 @@ public class Bin2Dec {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		//Allows user to input binary number
+		//Allows user to input binary
 		System.out.print("Enter a binary: ");
 		try {
 			System.out.println(ChapterTwelve.bin2Dec(input.nextLine()));
